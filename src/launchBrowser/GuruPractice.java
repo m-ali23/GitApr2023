ab2e0c32fb5b6fed18ff1d54189e17641d6802af
m-ali23 <142562812+m-ali23@users.noreply.github.com>
2023-08-20 21:38:53 -0500

    committed

---
+++ src/launchBrowser/GuruPractice.java
@@ -0,0 +1,59 @@
+package launchBrowser;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GuruPractice {
+
+	public static void main(String[] args) {
+		Scanner snc=new Scanner(System.in);
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/radio.html");					
+        WebElement radio1 = driver.findElement(By.id("vfb-7-1"));							
+        WebElement radio2 = driver.findElement(By.id("vfb-7-2"));							
+        		
+        //Radio Button1 is selected		
+        radio1.click();			
+        System.out.println("Radio Button Option 1 Selected");					
+        		
+        //Radio Button1 is de-selected and Radio Button2 is selected		
+        radio2.click();			
+        System.out.println("Radio Button Option 2 Selected");					
+        		
+        // Selecting CheckBox		
+        WebElement option1 = driver.findElement(By.id("vfb-6-0"));							
+
+        // This will Toggle the Check box 		
+        option1.click();			
+
+        // Check whether the Check box is toggled on 		
+        if (option1.isSelected()) {					
+            System.out.println("Checkbox is Toggled On");					
+
+        } else {			
+            System.out.println("Checkbox is Toggled Off");					
+        }		
+		driver.get("https://demo.guru99.com/test/facebook.html");					
+        WebElement chkFBPersist = driver.findElement(By.id("persist_box"));							
+        for (int i=0; i<2; i++) {											
+            chkFBPersist.click (); 			
+            System.out.println("Facebook Persists Checkbox Status is -  "+chkFBPersist.isSelected());							
+        }		
+		/*driver.get("http://demo.guru99.com/test/ajax.html");
+		driver.manage().window().maximize();
+		//driver.findElement(By.name("q")).sendKeys("selenium");
+		//driver.findElement(By.name("btnK")).click();
+		List<WebElement> elements = driver.findElements(By.name("name"));
+	    System.out.println("Number of elements:" +elements.size());
+
+	    for (int i=0; i<elements.size();i++){
+	      System.out.println("Radio button text:" + elements.get(i).getAttribute("value"));
+	}*/
+
+}
+}
---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,19 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) {
+		Scanner snc=new Scanner(System.in);
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		//driver.findElement(By.name("btnK")).click();
+	}
+}
